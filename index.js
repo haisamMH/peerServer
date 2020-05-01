3e3f5083bd66b2d6bbefebe3c130f2138533f542
Hysham <hyshamhashim@gmail.com>
2020-05-01 22:12:50 +0530

    com2

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.get('/', (req, res, next) => res.send('Hello world!'));
 const PORT = process.env.PORT || 9000;
 const server = app.listen(PORT);
 
-// const peerServer = ExpressPeerServer(server, {
-//   debug: true,
-//   path: '/myapp'
-// });
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: '/myapp'
+});
 
-// app.use('/peerjs', peerServer);
+app.use('/peerjs', peerServer);
