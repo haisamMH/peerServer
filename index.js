b2a59c0c1b9473b48071d8eb056d1981b8dd5c7e
Hysham <hyshamhashim@gmail.com>
2020-05-01 21:17:36 +0530

    com2

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ app.get('/', (req, res, next) => res.send('Hello world!'));
 
 // =======
 
-const server = app.listen(9000);
+// const server = app.listen(9000);
 
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
-  path: '/myapp'
-});
+// const peerServer = ExpressPeerServer(server, {
+//   debug: true,
+//   path: '/myapp'
+// });
 
-app.use('/peerjs', peerServer);
+// app.use('/peerjs', peerServer);
 
 // == OR ==
 
