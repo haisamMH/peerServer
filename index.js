5d7ed047575cb988afd513c8b6ec9a6060c7512e
Hysham <hyshamhashim@gmail.com>
2020-05-01 22:45:58 +0530

    com2

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const PORT = process.env.PORT || 9000;
+var fs = require('fs');
 var PeerServer = require('peer').PeerServer;
 
 var server = PeerServer({
     port: PORT,
-    path: '/peerjs'
+    path: '/peerjs',
+    ssl: {
+        key: fs.readFileSync('./cert/key.pem', 'utf8'),
+        cert: fs.readFileSync('./cert/cert.pem', 'utf8')
+    }
 });
